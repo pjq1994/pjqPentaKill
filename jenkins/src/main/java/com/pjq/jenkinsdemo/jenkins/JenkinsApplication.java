c7062cdad84a642e49ad5df6e46a0d74f7dcea25
panjiaqi <978461256@qq.com>
2021-04-10 16:24:10 +0800

    测试Jenkins自动构建

---
+++ jenkins/src/main/java/com/pjq/jenkinsdemo/jenkins/JenkinsApplication.java
@@ -0,0 +1,18 @@
+package com.pjq.jenkinsdemo.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+
+        Map map = new HashMap();
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
